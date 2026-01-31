2fb8b7ca2161685e58a830d9f2c7f6fb58e62a62
earthlingOnFire <78956999+earthlingOnFire@users.noreply.github.com>
2026-01-31 00:15:47 +0000

    feat: arm icon in hud now shows the wrong arm

---
+++ FrankenToilet/earthling/FistControlPatches.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using FrankenToilet.Core;
+using HarmonyLib;
+
+namespace FrankenToilet.earthling;
+
+[PatchOnEntry]
+[HarmonyPatch(typeof(FistControl))]
+public static class FistControlPatches
+{
+    [HarmonyPrefix]
+    [HarmonyPatch("UpdateFistIcon")]
+    public static void SwapArmsBeforeUpdatingIcon(FistControl __instance)
+    {
+        __instance.currentVarNum = (__instance.currentVarNum + 1) % 2;
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch("UpdateFistIcon")]
+    public static void SwapArmsAfterUpdatingIcon(FistControl __instance)
+    {
+        __instance.currentVarNum = (__instance.currentVarNum + 1) % 2;
+    }
+}
